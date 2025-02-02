d5eb589b21b44c2a7b307584c4ad174e37f87135
matthieu <matthieu.quernec@gmail.com>
2025-02-02 14:14:54 +0100

    .

--- 10_TP/search/Aggregator.cs
+++
@@ -1,7 +0,0 @@
-public class Aggregator    {
-    public int SizeMax { get; set; }
-    public int WeightMax { get; set; }
-    public int SizeMin { get; set; }
-    public int WeightMin { get; set; }
-
-}
--- 10_TP/search/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json;
-using System.IO;
-using BenchmarkDotNet.Running;
-
-
-var summary = BenchmarkRunner.Run<Bench10>();  
-    Console.WriteLine(summary);
-
